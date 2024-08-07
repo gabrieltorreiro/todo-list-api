ab091d17271d5426afb5c7688dbb7f4debc920fa
Gabriel Torreiro <glopestorreiro@gmail.com>
2024-08-07 01:31:59 -0300

    add task status tests

--- src/task/task.controller.spec.ts
+++ src/task/task.controller.spec.ts
@@ -15,6 +15,8 @@ describe('TaskController', () => {
           provide: TaskService,
           useValue: {
             findAll: jest.fn().mockResolvedValue([]),
+            doneTask: jest.fn().mockResolvedValue([]),
+            undoneTask: jest.fn().mockResolvedValue([]),
           },
         },
       ],
@@ -50,6 +52,68 @@ describe('TaskController', () => {
 
       // Assert
       expect(target).toEqual(tasks);
+    });
+  });
+
+  describe('doneTask', () => {
+    it('should return a 404 error when the task ID does not exist in the database', async () => {
+      // Arrange
+      const taskId = 9999;
+      jest
+        .spyOn(taskService, 'doneTask')
+        .mockRejectedValue(new Error('Task not found'));
+
+      // Act
+      const target = taskController.doneTask(taskId.toString());
+
+      // Assert
+      await expect(target).rejects.toThrow('Task not found');
+    });
+
+    it('should update the task status to "done" when the "/done" endpoint is called', async () => {
+      // Arrange
+      const taskId = 1;
+      const updatedTask: Partial<Task> = { id: taskId, status: true };
+      jest
+        .spyOn(taskService, 'doneTask')
+        .mockResolvedValue(updatedTask as Task);
+
+      // Act
+      const target = await taskController.doneTask(taskId.toString());
+
+      // Assert
+      expect(target).toEqual(updatedTask);
+    });
+  });
+
+  describe('undoneTask', () => {
+    it('should return a 404 error when the task ID does not exist in the database', async () => {
+      // Arrange
+      const taskId = 9999;
+      jest
+        .spyOn(taskService, 'undoneTask')
+        .mockRejectedValue(new Error('Task not found'));
+
+      // Act
+      const target = taskController.undoneTask(taskId.toString());
+
+      // Assert
+      await expect(target).rejects.toThrow('Task not found');
+    });
+
+    it('should update the task status to "undone" when the "/undone" endpoint is called', async () => {
+      // Arrange
+      const taskId = 1;
+      const updatedTask: Partial<Task> = { id: taskId, status: false };
+      jest
+        .spyOn(taskService, 'undoneTask')
+        .mockResolvedValue(updatedTask as Task);
+
+      // Act
+      const target = await taskController.undoneTask(taskId.toString());
+
+      // Assert
+      expect(target).toEqual(updatedTask);
     });
   });
 });
---
+++ src/task/task.service.spec.ts
@@ -0,0 +1,109 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { TaskService } from './task.service';
+import { NotFoundException } from '@nestjs/common';
+import { Task } from './entities/task.entity';
+
+describe('TaskService', () => {
+  let service: TaskService;
+  const mockTaskRepository = {
+    findOneBy: jest.fn(),
+    save: jest.fn(),
+  };
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        TaskService,
+        { provide: 'TASK_REPOSITORY', useValue: mockTaskRepository },
+      ],
+    }).compile();
+
+    service = module.get<TaskService>(TaskService);
+  });
+
+  describe('findOne', () => {
+    it('should throw an error when the task ID is not found in the database', async () => {
+      mockTaskRepository.findOneBy.mockResolvedValue(null);
+
+      const status = service.findOne(1);
+
+      await expect(status).rejects.toThrow(NotFoundException);
+      expect(mockTaskRepository.findOneBy).toHaveBeenCalledWith({ id: 1 });
+      expect(mockTaskRepository.save).not.toHaveBeenCalled();
+    });
+
+    it('should return the task when the task ID is found in the database', async () => {
+      const task: Task = {
+        create: new Date(),
+        id: 1,
+        name: 'task 1',
+        status: false,
+      };
+      mockTaskRepository.findOneBy.mockResolvedValue(task);
+
+      const returnedTask = await service.findOne(1);
+
+      expect(returnedTask).toEqual(task);
+      expect(mockTaskRepository.findOneBy).toHaveBeenCalledWith({ id: 1 });
+      expect(mockTaskRepository.save).not.toHaveBeenCalled();
+    });
+  });
+
+  describe('doneTask', () => {
+    it('should throw an error when trying to mark a non-existent task as done', async () => {
+      mockTaskRepository.findOneBy.mockResolvedValue(null);
+
+      await expect(service.doneTask(1)).rejects.toThrow(NotFoundException);
+      expect(mockTaskRepository.findOneBy).toHaveBeenCalledWith({ id: 1 });
+      expect(mockTaskRepository.save).not.toHaveBeenCalled();
+    });
+
+    it('should update the status of a task to done when the task ID is valid', async () => {
+      const task: Task = {
+        create: new Date(),
+        id: 1,
+        name: 'task 1',
+        status: false,
+      };
+      mockTaskRepository.findOneBy.mockResolvedValue(task);
+
+      const updatedTask = await service.doneTask(1);
+
+      expect(updatedTask.status).toBe(true);
+      expect(mockTaskRepository.findOneBy).toHaveBeenCalledWith({ id: 1 });
+      expect(mockTaskRepository.save).toHaveBeenCalledWith(
+        expect.objectContaining({ id: 1, status: true }),
+      );
+    });
+  });
+
+  describe('undoneTask', () => {
+    it('should throw an error when trying to mark a non-existent task as undone', async () => {
+      mockTaskRepository.findOneBy.mockResolvedValue(null);
+
+      const task = service.undoneTask(1);
+
+      await expect(task).rejects.toThrow(NotFoundException);
+      expect(mockTaskRepository.findOneBy).toHaveBeenCalledWith({ id: 1 });
+      expect(mockTaskRepository.save).not.toHaveBeenCalled();
+    });
+
+    it('should update the status of a task to undone when the task ID is valid', async () => {
+      const task: Task = {
+        create: new Date(),
+        id: 1,
+        name: 'task 6',
+        status: true,
+      };
+      mockTaskRepository.findOneBy.mockResolvedValue(task);
+
+      const updatedTask = await service.undoneTask(1);
+
+      expect(updatedTask.status).toBe(false);
+      expect(mockTaskRepository.findOneBy).toHaveBeenCalledWith({ id: 1 });
+      expect(mockTaskRepository.save).toHaveBeenCalledWith(
+        expect.objectContaining({ id: 1, status: false }),
+      );
+    });
+  });
+});
